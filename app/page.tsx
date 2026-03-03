555f3a134199085de7f8828fbd8a55f21594287c
Solar Print Process Pvt. Ltd. <spppl.in@gmail.com>
2026-03-03 12:18:39 +0530

    Create Next.js home page

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Welcome to the Next.js Home Page!</h1>
+            <p>Current Date and Time: 2026-03-03 06:48:11 UTC</p>
+        </div>
+    );
+};
+
+export default HomePage;
